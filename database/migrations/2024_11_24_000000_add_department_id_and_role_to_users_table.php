da7cda45b355c7d9c103a38dd280dd9c10fa0153
uzairsharif <uzairsharif800@gmail.com>
2025-06-14 22:59:46 +0500

    default user role was changed from admin to user

--- database/migrations/2024_11_24_000000_add_department_id_and_role_to_users_table.php
+++ database/migrations/2024_11_24_000000_add_department_id_and_role_to_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('department_id')->default('1');
-            $table->string('role')->default('admin');
+            $table->string('role')->default('user');
         });
     }
 
